41c5c7d3aae6b1308a31511cce6d1b63690e0922
Alexander Scheurer <mail@aspepex.net>
2015-03-23 13:24:57 +0100

    A3

---
+++ A3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace A3
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(Fib(10));
+
+            Console.ReadKey();
+        }
+
+        static int Fib(int x)
+        {
+            if (x <= 1)
+                return 1;
+            return Fib(x - 1) + Fib(x - 2);
+        }
+    }
+}
