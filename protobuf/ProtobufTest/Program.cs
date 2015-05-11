be38199abb11182e46091474254f4ca828e7d2c5
Alexander Scheurer <mail@aspepex.net>
2015-05-11 15:31:21 +0200

    Started protobuf stuff

---
+++ protobuf/ProtobufTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StudentTypes;
+
+
+namespace ProtobufTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var studentliste = new StudentList();
+            studentliste.students = new List<Student>();
+
+            var student = new Student();
+            student.firstname = "Hans";
+            student.lastname = "Blab";
+            student.matnr = "124123";
+
+            student.courses = new List<Course>();
+
+            var mycourse = new Course();
+            mycourse.name = "Gamestuff";
+            mycourse.desc = "Stuff with Games";
+
+            student.courses.Add(mycourse);
+            studentliste.students.Add(student);
+
+        }
+    }
+}
---
+++ protobuf/Student/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using ProtoBuf;
+
+namespace StudentTypes
+{
+    [ProtoContract]
+    public class Student
+    {
+        [ProtoMember(1)]
+        public string firstname;
+        [ProtoMember(2)]
+        public string lastname;
+        [ProtoMember(3)]
+        public string matnr;
+
+        [ProtoMember(4)]
+        public List<Course> courses;
+    }
+
+    [ProtoContract]
+    public class Course
+    {
+        [ProtoMember(1)]
+        public string name;
+        [ProtoMember(2)]
+        public string desc;
+    }
+
+    [ProtoContract]
+    public class StudentList
+    {
+        [ProtoMember(1)]
+        public List<Student> students;
+    }
+
+}
