e62fb89212a9fe84281d0116ade9971667798ab1
Alexander Scheurer <mail@aspepex.net>
2015-06-15 15:26:31 +0200

    Stuff

---
+++ EventDelegate/EventDelegate/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventDelegate
+{
+    // <Game Input Library>
+    internal delegate void PlayerMovedDelegate();
+
+    class InputHandler
+    {
+        public PlayerMovedDelegate OnUp;
+        public PlayerMovedDelegate OnDown;
+        public PlayerMovedDelegate OnLeft;
+        public PlayerMovedDelegate OnRight;
+
+        public InputHandler()
+        {
+        }
+
+        public void Run()
+        {
+            for (;;)
+            {
+                switch (Console.ReadKey().KeyChar)
+                {
+                    case 'w':
+                        OnUp();
+                        break;
+                    case 'a':
+                        OnLeft();
+                        break;
+                    case 's':
+                        OnDown();
+                        break;
+                    case 'd':
+                        OnRight();
+                        break;
+
+                }
+            }
+        }
+    }
+    // </Game Input Library>
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InputHandler myInputHandler = new InputHandler();
+            
+            myInputHandler.OnDown = DoTheDown;
+
+            myInputHandler.OnUp = delegate()
+            {
+                Console.WriteLine("UP!");
+            };
+
+            myInputHandler.OnLeft = () =>
+            {
+                Console.WriteLine("Left!");
+            };
+
+            myInputHandler.OnRight = delegate
+            {
+                Console.WriteLine("Reight!");
+            };
+
+            myInputHandler.Run();
+        }
+
+        private static void DoTheDown()
+        {
+            Console.WriteLine("Down!");
+        }
+    }
+}
---
+++ MyFirstWpf/MyFirstWpf/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyFirstWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int clicks;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            clicks = 0;
+            MyButton.Click += delegate(object sender, RoutedEventArgs routedEventArgs)
+            {
+                clicks++;
+                MyTextBox.Text = "Butten " + clicks;
+            };
+
+            this.MouseMove += delegate(object sender, MouseEventArgs args)
+            {
+                Point pt = args.GetPosition(this);
+                MyTextBox.Text = "Maus bei " + pt;
+            };
+        }
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            clicks++;
+            MyTextBox.Text = "Butten " + clicks;
+        }
+    }
+}
