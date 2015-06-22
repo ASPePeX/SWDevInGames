191594ff693230ec259f4416df7d1caa1f9a879c
Alexander Scheurer <mail@aspepex.net>
2015-06-22 14:22:43 +0200

    Threading dummy

---
+++ Threading/Threading/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threading
+{
+    class Program
+    {
+        [DllImport("kernel32.dll")]
+        private static extern int GetCurrentProcessorNumber();
+
+        static void Main(string[] args)
+        {
+            Thread worker = new Thread(Blub);
+            worker.Start();
+
+            Console.WriteLine(GetCurrentProcessorNumber());
+
+        }
+
+        public static void Blub()
+        {
+            Console.WriteLine(GetCurrentProcessorNumber());
+        }
+
+    }
+}
